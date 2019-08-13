50ef5416fc884dbce5dd5d2708d25b108320df3e
DeyaaMaali <deyaa.maali.com>
2019-08-13 23:08:16 +0300

    more css on suggested vidoes list

--- React/youtube/src/Components/VideoItem.js
+++ React/youtube/src/Components/VideoItem.js
@@ -21,7 +21,8 @@ changeCursor = () => {
   return (
     <>
     <dev className="item" onClick={this.props.openVideo.bind(this,this.props.items)} >
-    <div ><img src={this.props.items.snippet.thumbnails.default.url} alt="thumbnails"/></div>
+    <div ><img style={{    maxWidth: "180px",
+    maxHeight: "200px" }} src={this.props.items.snippet.thumbnails.medium.url} alt="thumbnails"/></div>
     <div >{this.props.items.snippet.title}</div>
     </dev>
     <hr/>
